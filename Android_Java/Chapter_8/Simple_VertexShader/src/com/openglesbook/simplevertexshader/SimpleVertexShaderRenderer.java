e586260c1e81855eba8d92ff5d02a3c8dea84163
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2021-09-09 15:06:02 +0800

    1、调通将顶点着色器和片段着色器  抽取为单独的文件，然后加载

--- Android_Java/Chapter_8/Simple_VertexShader/src/com/openglesbook/simplevertexshader/SimpleVertexShaderRenderer.java
+++ Android_Java/Chapter_8/Simple_VertexShader/src/com/openglesbook/simplevertexshader/SimpleVertexShaderRenderer.java
@@ -51,6 +51,7 @@ import android.opengl.GLSurfaceView;
 import android.os.SystemClock;
 
 public class SimpleVertexShaderRenderer implements GLSurfaceView.Renderer {
+    private final Context mContext;
     // Handle to a program object
     private int mProgramObject;
 
@@ -58,13 +59,13 @@ public class SimpleVertexShaderRenderer implements GLSurfaceView.Renderer {
     private int mMVPLoc;
 
     // Vertex data
-    private ESShapes mCube = new ESShapes();
+    private final ESShapes mCube = new ESShapes();
 
     // Rotation angle
     private float mAngle;
 
     // MVP matrix
-    private ESTransform mMVPMatrix = new ESTransform();
+    private final ESTransform mMVPMatrix = new ESTransform();
 
     // Additional Member variables
     private int mWidth;
@@ -75,37 +76,17 @@ public class SimpleVertexShaderRenderer implements GLSurfaceView.Renderer {
     // Constructor
     //
     public SimpleVertexShaderRenderer(Context context) {
-
+        mContext = context;
     }
 
     ///
     // Initialize the shader and program object
     //
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
-        String vShaderStr =
-                "#version 300 es 							 \n" +
-                "uniform mat4 u_mvpMatrix;                   \n" +
-                "layout(location = 0) in vec4 a_position;    \n" +
-                "layout(location = 1) in vec4 a_color;       \n" +
-                "out vec4 v_color;                           \n" +
-                "void main()                                 \n" +
-                "{                                           \n" +
-                "   v_color = a_color;                       \n" +
-                "   gl_Position = u_mvpMatrix * a_position;  \n" +
-                "}                                           \n";
-
-        String fShaderStr =
-                "#version 300 es 							 \n" +
-                "precision mediump float;                    \n" +
-                "in vec4 v_color;                            \n" +
-                "layout(location = 0) out vec4 outColor;     \n" +
-                "void main()                                 \n" +
-                "{                                           \n" +
-                "  outColor = v_color;                       \n" +
-                "}                                           \n";
-
         // Load the shaders and get a linked program object
-        mProgramObject = ESShader.loadProgram(vShaderStr, fShaderStr);
+        mProgramObject = ESShader.loadProgramFromAsset(mContext,
+                "shaders/vertexShader.vert",
+                "shaders/fragmentShader.frag");
 
         // Get the uniform locations
         // mvp矩阵 用 glGetUniformLocation加载到统一变量中，供着色器使用
